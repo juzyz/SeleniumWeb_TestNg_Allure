064cb224ea40d0df251ce490ae671ff46eee0050
Iuliia Rudi <connection454@gmail.com>
2023-11-23 02:34:30 -0500

    deleted unnecessary import

--- src/main/java/pages/base/BasePage.java
+++ src/main/java/pages/base/BasePage.java
@@ -3,7 +3,6 @@ package pages.base;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import java.time.Duration;
